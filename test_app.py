6179c2147cc6eb4cbda12a693838f5d0d9c36144
Victor Tedesco <victor.tedesco10@gmail.com>
2026-05-13 20:48:14 -0300

    Add unit tests for the app

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app import app
+class TestApp(unittest.TestCase):
+ def setUp(self):
+ self.app = app.test_client()
+ def test_home(self):
+ response = self.app.get('/')
+ self.assertEqual(response.data.decode('utf-8'), "SISTEMA ONLINE V1.0")
+if __name__ == '__main__':
+ unittest.main()
