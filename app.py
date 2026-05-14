a045ec26eecdf01da82a17eb6eedcc4eaec73179
Victor Tedesco <victor.tedesco10@gmail.com>
2026-05-13 21:18:29 -0300

    (fix) * py files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route('/')
 def home():
- return "SISTEMA ONLINE V1.0"
+    return "SISTEMA ONLINE V1.0"
+
 if __name__ == "__main__":
- app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- test_app.py
+++ test_app.py
@@ -1,10 +1,13 @@
 import unittest
 from app import app
+
 class TestApp(unittest.TestCase):
- def setUp(self):
- self.app = app.test_client()
- def test_home(self):
- response = self.app.get('/')
- self.assertEqual(response.data.decode('utf-8'), "SISTEMA ONLINE V1.0")
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.data.decode('utf-8'), "SISTEMA ONLINE V1.0")
+
 if __name__ == '__main__':
- unittest.main()
+    unittest.main()
